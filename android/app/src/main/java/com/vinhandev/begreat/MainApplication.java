8cbdbd77872ac24f6e8ee2a75229896c0ca7b5e8
vinhandev <vinhan.dev@gmail.com>
2023-12-10 14:47:58 +0700

    update: change name source code

--- android/app/src/main/java/com/vinhandev/begreat/MainApplication.java
+++ android/app/src/main/java/com/vinhandev/begreat/MainApplication.java
@@ -1,4 +1,4 @@
-package com.shareshareapp;
+package com.vinhandev.begreat;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/BeGreatTests/BeGreatTests.m
+++ ios/BeGreatTests/BeGreatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ShareShareAppTests : XCTestCase
+@interface BeGreatTests : XCTestCase
 
 @end
 
-@implementation ShareShareAppTests
+@implementation BeGreatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
