bba832bf246c130cf1c5fcbd7fa6f64080889274
vinhandev <vinhan.dev@gmail.com>
2023-12-13 19:27:00 +0700

    update: change color config

--- src/theme/_config.ts
+++ src/theme/_config.ts
@@ -1,40 +1,36 @@
-import { Dimensions } from 'react-native'
-import { DarkTheme } from "@react-navigation/native";
+import { Dimensions } from 'react-native';
+import { DarkTheme } from '@react-navigation/native';
 
-import type { ThemeConfiguration } from "@/types/theme/config";
+import type { ThemeConfiguration } from '@/types/theme/config';
 
 const colorsLight = {
-  black500: "#000000BB",
-  black300: "#333333",
-  black100: "#424242",
-  white: "#ffffff",
-  grey: "#B0B0B0",
-  grey2: '#D9D9D9',
-  pink900: "#A11660",
-  pink600: "#D91D81",
-  pink400: "#E877B3",
-  green: "#558336",
-  red: "#FE1B1B",
+  black: '#000000',
+  white: '#ffffff',
+  primary: '#27374D',
+  secondary: '#526D82',
+  tertiary: '9DB2BF',
+  quaternary: '9DB2BF',
+  purple: '#7B66FF',
+  success: '#C5E898',
+  red: '#F05454',
 } as const;
 
 const colorsDark = {
-  black500: "#000000AA",
-  black300: "#333333",
-  black100: "#424242",
-  white: "#ffffff",
-  grey: "#B0B0B0",
-  grey2: '#D9D9D9',
-  pink900: "#A11660",
-  pink600: "#D91D81",
-  pink400: "#E877B3",
-  green: "#558336",
-  red: "#FE1B1B",
+  black: '#000000',
+  white: '#ffffff',
+  primary: '#27374D',
+  secondary: '#526D82',
+  tertiary: '9DB2BF',
+  quaternary: '9DB2BF',
+  purple: '#7B66FF',
+  success: '#C5E898',
+  red: '#F05454',
 } as const;
 
 export const dimensionSize = {
   screen: Dimensions.get('screen'),
-  window: Dimensions.get('window')
-}
+  window: Dimensions.get('window'),
+};
 export const metricsSize = [10, 15, 20] as const;
 const fontSize = [14, 16, 18, 20, 40] as const;
 
