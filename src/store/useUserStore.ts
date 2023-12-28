574392280d8891126f8cbf986f89d4a12e05d6c8
vinhandev <vinhan.dev@gmail.com>
2023-12-28 08:13:11 +0700

    use day data

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,22 +1,41 @@
-import { create } from 'zustand'
-import { createJSONStorage, persist } from 'zustand/middleware'
-import { MMKVStorage } from './mmkv'
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { MMKVStorage } from './mmkv';
 
+type UserDayType = {
+  face: string;
+  body: string;
+  weight: number;
+  height: number;
+  date: Date;
+  meals: {
+    name: string;
+    description: string;
+    image: string;
+    calories: number;
+  }[];
+};
 type InitState = {
-    isLoading: boolean,
-    loadingMessage: string,
-    setLoadingIn: (loadingMessage: string) => void
-    setLoadingOut: () => void
-}
+  listOfDays: UserDayType[];
+  isLoading: boolean;
+  loadingMessage: string;
+  setLoadingIn: (loadingMessage: string) => void;
+  setLoadingOut: () => void;
+};
 
 export const useUserStore = create<InitState>()(
-    persist((set) => ({
-        isLoading: false,
-        loadingMessage: '',
-        setLoadingIn: (loadingMessage: string) => set(() => ({ isLoading: true, loadingMessage })),
-        setLoadingOut: () => set(() => ({ isLoading: false }))
-    }), {
-        name: 'user-settings-storage',
-        storage: createJSONStorage(() => MMKVStorage)
-    })
-)
+  persist(
+    (set) => ({
+      listOfDays: [],
+      isLoading: false,
+      loadingMessage: '',
+      setLoadingIn: (loadingMessage: string) =>
+        set(() => ({ isLoading: true, loadingMessage })),
+      setLoadingOut: () => set(() => ({ isLoading: false })),
+    }),
+    {
+      name: 'user-settings-storage',
+      storage: createJSONStorage(() => MMKVStorage),
+    }
+  )
+);
